8fe2a143dc14d8e81042e61ebd2ca370565dd500
Viikklly <94692215+Viikklly@users.noreply.github.com>
2026-04-14 21:18:11 +0300

    Настройка контейнеров

--- services/config-service/src/main/java/com/restaurant/config_service/ConfigServiceApplication.java
+++ services/config-service/src/main/java/com/restaurant/config_service/ConfigServiceApplication.java
@@ -2,8 +2,14 @@ package com.restaurant.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/*
+ Централизованные конфигурации
+ */
 
 @SpringBootApplication
+@EnableConfigServer  // сервер для раздачи конфигураций
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
--- services/discovery-service/src/main/java/com/restaurant/discovery_service/DiscoveryServiceApplication.java
+++ services/discovery-service/src/main/java/com/restaurant/discovery_service/DiscoveryServiceApplication.java
@@ -2,8 +2,14 @@ package com.restaurant.discovery_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+Реестр микросервисов
+ */
 
 @SpringBootApplication
+@EnableEurekaServer  // сервер для регистрации всех микросервисов
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
