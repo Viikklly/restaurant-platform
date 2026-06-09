e4b0d4c1a6ad9c58418a49db832ec71a725ee9b7
Viikklly <94692215+Viikklly@users.noreply.github.com>
2026-06-09 13:42:14 +0300

    NotificationService добавлена нотификация приготовления блюда

--- services/notification-service/src/main/java/com/restaurant/notification_service/service/NotificationService.java
+++ services/notification-service/src/main/java/com/restaurant/notification_service/service/NotificationService.java
@@ -16,31 +16,50 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class NotificationService {
 
-    /// Слушаем топик с заказами
+    /**
+    * Слушаем топик, что стартует приготовления блюда
+     */
+    @KafkaListener(topics = "kitchen-service.cooking.started", groupId = "notification-group")
+    public void handleCookingStarted(OrderCreatedEvent event) {
+        log.info("(NotificationService) УВЕДОМЛЕНИЕ: Заказ #{} начали готовить! Скоро будет готов.", event.getOrderId());
+    }
+
+    /**
+    * Слушаем топик с заказами
+     */
     @KafkaListener(topics = "order-service.order.created", groupId = "notification-group")
     public void handleOrderCreated(OrderCreatedEvent event) {
         log.info("(NotificationService) УВЕДОМЛЕНИЕ: Заказ #{} создан на сумму {} руб.",
                 event.getOrderId(), event.getTotalAmount());
     }
 
-    /// Слушаем топик с платежами
+    /**
+     *  Слушаем топик с платежами
+     */
     @KafkaListener(topics = "payment-service.payment.success", groupId = "notification-group")
     public void handlePaymentSuccess(PaymentProcessedEvent event) {
         log.info("(NotificationService) УВЕДОМЛЕНИЕ: Заказ #{} успешно оплачен!", event.getOrderId());
     }
 
+    /**
+     * Слушаем заказ не оплачен
+     */
     @KafkaListener(topics = "payment-service.payment.failed", groupId = "notification-group")
     public void handlePaymentFailed(PaymentProcessedEvent event) {
         log.info("(NotificationService) УВЕДОМЛЕНИЕ: Заказ #{} НЕ ОПЛАЧЕН: {}", event.getOrderId(), event.getMessage());
     }
 
-    /// Слушаем топик с кухней
+    /**
+    * Слушаем топик с кухней
+     */
     @KafkaListener(topics = "order-service.order.paid", groupId = "notification-group")
     public void handleOrderPaid(Long orderId) {
         log.info("(NotificationService) УВЕДОМЛЕНИЕ: Заказ #{} передан на кухню и готовится!", orderId);
     }
 
-    /// Слушаем топик с готовыми заказами от кухни
+    /**
+    * Слушаем топик с готовыми заказами от кухни
+     */
     @KafkaListener(topics = "kitchen-service.order.ready", groupId = "notification-group")
     public void handleOrderReady(KitchenOrderReadyEvent event) {
         log.info("(NotificationService) УВЕДОМЛЕНИЕ: Заказ #{} ГОТОВ к выдаче.",
---
+++ services/payment-service/src/main/java/com/restaurant/payment_service/controller/PaymentController.java
@@ -0,0 +1,4 @@
+package com.restaurant.payment_service.controller;
+
+public class PaymentController {
+}
